5f7c1a0e93d4d18ba39ecb2c1480373806f71ca1
Christopher Young <christopheryoung@MacBook123.local>
2024-08-28 15:48:59 -0700

    added socket connection

--- server.js
+++ server.js
@@ -2,14 +2,28 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import "./db/connection.js";
+// Sockets
+import { createServer } from "http";
+import { Server } from "socket.io";
 
 const PORT = process.env.PORT || 5050;
 const app = express();
+const server = createServer(app);
+const io = new Server(server, {
+  // Allow connection from client
+  cors: { origin: '*' }
+});
 
+//----- Middleware
 app.use(cors());
 app.use(express.json());
 
-// start the Express server
-app.listen(PORT, () => {
+//----- Socket events
+io.on("connection", socket => {
+  console.log(`New connection: ${socket.id}`);
+});
+
+//----- Server connection
+server.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
